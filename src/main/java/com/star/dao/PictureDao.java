5620d1322baef79a8dc6805d1a173c1c136f9821
change <690657326@qq.com>
2021-09-26 16:21:51 +0800

    上传博客

---
+++ src/main/java/com/star/dao/PictureDao.java
@@ -0,0 +1,34 @@
+package com.star.dao;
+
+import com.star.entity.Picture;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Description: 照片墙持久层接口
+ * @Date: Created in 23:32 2020/4/16
+ * @Author: ONESTAR
+ * @QQ群: 530311074
+ * @URL: https://onestar.newstar.net.cn/
+ */
+@Mapper
+@Repository
+public interface PictureDao {
+
+    List<Picture> listPicture();
+
+    int savePicture(Picture picture);
+
+    Picture getPicture(Long id);
+
+    int updatePicture(Picture picture);
+
+    void deletePicture(Long id);
+
+    @Select("select * from t_picture order by t_picture.id desc limit 10")
+    List<Picture> listPictureList();
+
+}
